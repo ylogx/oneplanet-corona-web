18dc04fb5e8c107cadf3fd6ee9f3a3eaf6964d63
Shubham Chaudhary <shubham@chaudhary.xyz>
2020-03-30 00:23:45 -0400

    Remove unused files

--- app/controllers/router.go
+++ app/controllers/router.go
@@ -8,7 +8,6 @@ func SetupRouter() *gin.Engine {
 	r := gin.Default()
 	r.GET("/ping", Ping)
 
-	//r.GET("/home", GetDataForHomepage)
 	r.Static("/static", "./static")
 	r.StaticFile("/privacy", "./static/privacy_policy.html")
 
